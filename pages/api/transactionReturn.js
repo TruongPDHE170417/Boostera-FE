ef6dd00c0b04e856d233d87ef591605eee18463c
TruongPDHE170417 <truongpdhe170417@fpt.edu,vn>
2024-02-29 22:19:15 +0700

    develop integrated vnpay

---
+++ pages/api/transactionReturn.js
@@ -0,0 +1,32 @@
+//handle when vnpay redirect back
+import queryString from 'qs';
+
+const handler = async (req, res) => {
+    const uiRedirect = "become-booster" //after handle, what we will handle
+    try {
+        //find a way to send it to server   
+        let vnpParam = await req.query;
+        let serverApi = "http://localhost:9999/payment/vnpay_return"
+        serverApi += '?' + queryString.stringify(vnpParam, { encode: false })
+        const transactionStatus = vnpParam['vnp_TransactionStatus'];
+        if (transactionStatus !== "00") {
+
+        } else if (transactionStatus == "00") {
+            const response = await fetch(serverApi, {
+                method: "GET",
+                mode: "cors",
+            });
+            const data = await response.json();
+            res.redirect(`/${uiRedirect}`);
+        } else {
+            throw new Error("Unknown transactionStatus code: " + transactionStatus);
+        }
+
+    } catch (error) {
+        res.status(500).json({
+            error: error.toString()
+        });
+    }
+};
+
+export default handler;
