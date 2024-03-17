4ae971739acd4f83501d858e4c385cdcc6cf5e51
TruongPDHE170417 <truongpdhe170417@fpt.edu,vn>
2024-03-17 19:04:01 +0700

    develop change password

--- pages/forgot-password.tsx
+++ pages/forgot-password.tsx
@@ -1,31 +1,91 @@
-import React from 'react'
-import Link from 'next/link'
-import CheckBox from '@components/common/CheckBox'
-import CustomButton from '@components/common/CustomButton'
-import CustomInput from '@components/common/CustomInput'
-import ChevronIcon from '@components/icons/ChevronIcon'
+import React, { ChangeEvent, useState } from "react"
+import Link from "next/link"
+import CheckBox from "@components/common/CheckBox"
+import CustomButton from "@components/common/CustomButton"
+import CustomInput from "@components/common/CustomInput"
+import ChevronIcon from "@components/icons/ChevronIcon"
+import { string } from "zod"
 
+export type ChangePassRequest = {
+  oldPass: string
+  newPass: string
+  reEnterPass: string
+}
 const ForgotPassword = () => {
+  const [changePass, setChangePass] = useState<ChangePassRequest>({
+    oldPass: "",
+    newPass: "",
+    reEnterPass: "",
+  })
+  const [errorMessage, setErrorMessage] = useState<ChangePassRequest>({
+    oldPass: "",
+    newPass: "",
+    reEnterPass: "",
+  })
+
+  const handleChangeInput = (e: ChangeEvent<HTMLInputElement>) => {
+    const name = e.target.name
+    const value = e.target.value
+    setChangePass({
+      ...changePass,
+      [name]: value,
+    })
+  }
+
+  const validateForm = () =>{
+
+  }
+
+  const hanldeSubmit = (e: React.FormEvent) => {
+    e.preventDefault()
+    console.log(changePass)
+    if(validateForm()){
+      
+    }
+  }
+
   return (
     <div className="hero min-h-screen bg-theme">
-      <div className="flex gap-12 items-center">
-        <div className="w-1/2 card shadow-2xl hidden lg:flex justify-center items-center p-8">
+      <div className="flex items-center gap-12">
+        <div className="card hidden w-1/2 items-center justify-center p-8 shadow-2xl lg:flex">
           <img src="images/auth-forgot-password.png" alt="" className="w-[60%]" />
         </div>
-        <div className="shrink-0 w-full lg:w-1/2 max-w-sm px-8 lg:px-2">
-          <div className="text-center lg:text-left text-slate-400">
-            <h1 className="text-5xl font-bold">Forgot Password? 🔒</h1>
-            <p className="py-6">Enter your email and we′ll send you instructions to reset your password</p>
+        <div className="w-full max-w-sm shrink-0 px-8 lg:w-1/2 lg:px-2">
+          <div className="text-center text-slate-400 lg:text-left">
+            <h1 className="text-4xl font-bold">Change Password 🔒</h1>
+            <p className="py-6">Change your pass to a more secure one</p>
           </div>
-          <div className="flex flex-col gap-4">
-            <CustomInput label="Email" placeholder='user@email.com' />
-            <CustomButton label="Send resend link" />
-            <div className="flex gap-2 justify-center">
-              <Link href="login" className="text-primary-400 hover:underline">
-                Back to login
-              </Link>
+          <form onSubmit={hanldeSubmit}>
+            <div className="flex flex-col gap-4">
+              <CustomInput
+                type="password"
+                name="oldPass"
+                label="Old password"
+                placeholder="Old password"
+                onChange={handleChangeInput}
+              />
+              <CustomInput
+                type="password"
+                name="newPass"
+                label="New password"
+                placeholder="New password"
+                onChange={handleChangeInput}
+              />
+              <CustomInput
+                type="password"
+                name="reEnterPass"
+                label="Re-enter new password"
+                placeholder="Re-enter new password"
+                onChange={handleChangeInput}
+              />
+              <CustomButton label="Change" />
+              <div className="flex justify-center gap-2">
+                <Link href="login" className="text-primary-400 hover:underline">
+                  Back to login
+                </Link>
+              </div>
             </div>
-          </div>
+          </form>
         </div>
       </div>
     </div>
--- src/components/common/CustomInput/index.tsx
+++ src/components/common/CustomInput/index.tsx
@@ -1,7 +1,8 @@
-import { memo } from 'react'
+import { memo } from "react"
 
 type Props = {
   type?: string
+  name?: string
   label?: string
   placeholder?: string
   id?: string
@@ -12,16 +13,30 @@ type Props = {
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void
 }
 
-const CustomInput = ({ type = 'text', label, placeholder, id, value, isError, isDisable, customClassName, onChange }: Props) => {
+const CustomInput = ({
+  type = "text",
+  label,
+  placeholder,
+  id,
+  value,
+  isError,
+  isDisable,
+  customClassName,
+  onChange,
+  name = "",
+}: Props) => {
   return (
     <div className="w-full">
-      {label &&
-        <label htmlFor={id} className={`block mb-2 font-medium text-sm ${isError ? 'text-red-600' : 'text-slate-400'}`}>{label}</label>
-      }
+      {label && (
+        <label htmlFor={id} className={`mb-2 block text-sm font-medium ${isError ? "text-red-600" : "text-slate-400"}`}>
+          {label}
+        </label>
+      )}
       <input
+        name={name}
         type={type}
         id={id}
-        className={`${isError ? 'text-red-400 border border-red-500 focus:outline-red-700' : ' border border-slate-500 focus:border-primary-400'} bg-theme text-slate-300 placeholder:text-slate-600 text-sm rounded-lg focus-visible:outline-none block w-full p-2.5 ${customClassName}`}
+        className={`${isError ? "border border-red-500 text-red-400 focus:outline-red-700" : " border border-slate-500 focus:border-primary-400"} block w-full rounded-lg bg-theme p-2.5 text-sm text-slate-300 placeholder:text-slate-600 focus-visible:outline-none ${customClassName}`}
         placeholder={placeholder}
         disabled={isDisable}
         value={value}
